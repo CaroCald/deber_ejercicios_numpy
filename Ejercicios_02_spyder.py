be287cfbd61f7882278b9f3e3858b065b3fc8d0c
Carolina Calderon <diana.calderon@epn.edu.ec>
2018-11-19 18:57:28 -0500

    ejercicios

---
+++ Ejercicios_02_spyder.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 18:37:50 2018
+
+@author: Carolina Calderon
+"""
+
+import numpy as np
+import pandas as pd
+
+CSV_PATH = "C://Users//Carolina//Documents//GitHub//deber_ejercicios_numpy//data//notas_alumnos.csv"
+
+notas_alumnos = pd.read_csv(CSV_PATH,encoding='latin-1')
+
+# Generar tabla de frecuencias
+tab = pd.crosstab(index=notas_alumnos["nota"],columns="frecuencia")
+print(tab)
+
+
+
+
+                                
